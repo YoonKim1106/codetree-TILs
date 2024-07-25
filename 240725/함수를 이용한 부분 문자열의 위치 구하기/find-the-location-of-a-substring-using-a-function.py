c0a56846b149f54f374105ab355613298e6a8edd
YoonKim <146703184+YoonKim1106@users.noreply.github.com>
2024-07-25 17:20:56 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240725/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,7 @@
+def find_substring(input_string, target_string):
+    return input_string.find(target_string)
+
+input_string = input()
+target_string = input()
+
+print(find_substring(input_string,target_string))
