8d1434485edbf49dc248e35969ee92b6b108ccab
YoonKim <146703184+YoonKim1106@users.noreply.github.com>
2024-07-27 19:42:31 +0900

    Title: 두 사람, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/두 사람/two-person.py
+++ 240727/두 사람/two-person.py
@@ -8,7 +8,7 @@ person1_s = person1_info[1]
 person2_s = person2_info[1]
 
 def check(person1_a,person1_s,person2_a,person2_s):
-    if (person1_a > 19 and person1_s == 'M') or (person2_a > 19 and person1_s == 'M'):
+    if (person1_a >= 19 and person1_s == 'M') or (person2_a >= 19 and person2_s == 'M'):
         print(1)
     else:
         print(0)
