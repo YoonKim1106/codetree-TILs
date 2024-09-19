2fbd23f1924c3fff1fc137aac6bbaed34cf72524
YoonKim <146703184+YoonKim1106@users.noreply.github.com>
2024-09-19 11:54:29 +0900

    Title: 특정 숫자 맞추기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240919/특정 숫자 맞추기/catching-specific-number.py
@@ -0,0 +1,10 @@
+while True:
+    t = 25
+    num = int(input())
+    if num > 25:
+        print('Lower')
+    elif num < 25:
+        print('Higher')
+    else:
+        print("Good")
+        break
