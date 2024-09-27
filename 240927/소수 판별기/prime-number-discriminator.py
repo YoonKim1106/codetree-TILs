bd7542f12aaf18e2f4526dae749e1b42de2ecb98
YoonKim <146703184+YoonKim1106@users.noreply.github.com>
2024-09-27 17:48:02 +0900

    Title: 소수 판별기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240927/소수 판별기/prime-number-discriminator.py
+++ 240927/소수 판별기/prime-number-discriminator.py
@@ -1,14 +1,14 @@
 n = int(input())
 
-def aaa(n):
-    cnt = 0
-    for i in range(1,n+1):
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            cnt += 1
-    
-    if cnt == 2:
-        print('P')
-    else:
-        print('C')
+            return False
+    return True
 
-aaa(n)
+if is_prime(n):
+    print('P')
+else:
+    print('C')
